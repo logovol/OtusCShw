1eb608f88045853886094d477c0b60984958335a
logovol <pasha1985@inbox.ru>
2024-06-07 23:20:02 +0300

    Домашняя работа выполнена

---
+++ Events/Program.cs
@@ -0,0 +1,32 @@
+﻿
+DirectoryWalker dirWalker = new DirectoryWalker();
+
+// Подписка на событие
+dirWalker.FileFound += (sender, e) =>
+{
+    Console.WriteLine($"File found {e.FileName}");
+
+    if (e.FileName.EndsWith(".txt"))
+    {
+        Console.WriteLine("Searching canceled. txt file was founded");
+        e.Cancel = true;
+    }
+};
+
+string dirPath = @"C:\Users\logov\Desktop\doc";
+dirWalker.Walk(dirPath);
+
+#region FileArgs
+public class FileArgs : EventArgs
+{
+    public string FileName { get; }
+
+    public bool Cancel { get; set; }
+
+    public FileArgs(string fileName)
+    {
+        FileName = fileName;
+        Cancel = false;
+    }
+}
+#endregion
---
+++ HomeTask/EnumerableExtensions.cs
@@ -0,0 +1,34 @@
+﻿public static class EnumerableExtensions
+{
+    public static T GetMax<T>(this IEnumerable<T> collection, Func<T, float> convertToNumber) where T : class
+    {
+        if (collection == null) throw new ArgumentNullException(nameof(collection));
+        if (convertToNumber == null) throw new ArgumentNullException(nameof(convertToNumber));
+                
+
+        using (var enumerator = collection.GetEnumerator())
+        {
+            if (!enumerator.MoveNext())
+            {
+                throw new InvalidOperationException("Sequence contains no elements");
+            }
+
+            T maxElement = enumerator.Current;
+            float maxValue = convertToNumber(maxElement);
+
+            while (enumerator.MoveNext())
+            {
+                T currentElement = enumerator.Current;
+                float currentValue = convertToNumber(currentElement);
+
+                if (currentValue > maxValue)
+                {
+                    maxElement = currentElement;
+                    maxValue = currentValue;
+                }
+            }
+
+            return maxElement;
+        }
+    }
+}
