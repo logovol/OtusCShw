0ff7383b53b74a83643ded9989f293b6d6c00c49
logovol <pasha1985@inbox.ru>
2024-06-07 23:20:02 +0300

    Домашняя работа выполнена

---
+++ Events/DirectoryWalker.cs
@@ -0,0 +1,60 @@
+﻿public class DirectoryWalker
+{
+    // Событие, вызывающееся при нахождении каждого файла 
+    public event EventHandler<FileArgs>? FileFound;
+
+
+    public void Walk(string dir)
+    {
+        if (string.IsNullOrEmpty(dir))
+        {
+            throw new ArgumentException("The directory path cannot be null or empty.", nameof(dir));
+        };
+
+        if (!Directory.Exists(dir))
+        {
+            throw new DirectoryNotFoundException($"The directory '{dir}' does not exist.");
+        }
+
+        WalkDirectory(dir);
+    }
+
+    private void WalkDirectory(string dir)
+    {
+        try
+        {
+            foreach (var filePath in Directory.GetFiles(dir))
+            {
+                var args = new FileArgs(filePath);
+                OnFileFound(args);
+
+                if (args.Cancel)
+                {
+                    Console.WriteLine("Searcing canceled");
+                    return;
+                }
+
+            }
+
+            foreach (var subDir in Directory.GetDirectories(dir))
+            {
+                WalkDirectory(subDir);
+            }
+
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.WriteLine($"Access denied to directory '{dir}': {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while accessing directory '{dir}': {ex.Message}");
+        }
+    }
+
+    // Метод для вызова события FileFound
+    protected virtual void OnFileFound(FileArgs e)
+    {
+        FileFound?.Invoke(this, e);
+    }
+}
--- HomeTask/Program.cs
+++ HomeTask/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+public class Product
+{
+    public string? Name { get; set; }
+    public float Weight { get; set; }    
+}
+
+class Program
+{
+    
+    static void Main()
+    {
+        var products = new List<Product>
+        {
+            new Product { Name = "Melon", Weight = 1.2f},
+            new Product { Name = "Water melon", Weight= 0.8f},
+            new Product { Name = "Ginger", Weight = 2.5f},
+            new Product { Name = "Mango", Weight = 3.0f}
+        };
+
+        Func<Product, float> del = GetPrice;
+        //Product maxPricedProduct = products.MaxBy(p => p.Sale);
+        Product maxPricedProduct = products.GetMax(del);
+        
+        Console.WriteLine($"Product with max weight: {maxPricedProduct.Name} {maxPricedProduct.Weight} kg.");
+                
+        float GetPrice(Product p)
+        {
+            return p.Weight;
+        };
+
+    }
+}
