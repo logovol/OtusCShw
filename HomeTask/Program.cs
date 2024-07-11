0ebc625ea51552c8351f682ad55de92c0b97257e
logovol <pasha1985@inbox.ru>
2024-07-11 11:54:38 +0300

    Домашнее задание по теме Task выполнено

--- HomeTask/Program.cs
+++ HomeTask/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+Stopwatch sw = Stopwatch.StartNew();
+// Путь к директории
+string path = @"C:\Users\logov\Desktop\testtext";
+// Файлы в директории
+string[] files = Directory.GetFiles(path);
+
+// Создается массив задач, каждая из которых читает файл и считает пробелы в нем.
+// Метод Select используется для создания задач с помощью лямбда-функции.
+Task<int>[] tasks = files.Select(files => CountSpacesinFileAsync(files)).ToArray();
+// Ожидание завершения всех задач с использованием метода Task.WhenAll,
+// который возвращает массив результатов выполнения всех задач.
+int[] results = await Task.WhenAll(tasks);
+
+sw.Stop();
+Console.WriteLine($"Обнаружено {results.Sum()} пробелов");
+Console.WriteLine($"Отработало за {sw.Elapsed.TotalSeconds} секунд");
+
+// Асинхронный метод подсчета пробела в файле
+static async Task<int> CountSpacesinFileAsync(string filePath)
+{  
+    string text = await File.ReadAllTextAsync(filePath);
+    return text.Count(x  => x == ' ');
+}
